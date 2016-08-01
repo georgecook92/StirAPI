226bf7d6688fa017d56d8242e8cea6b8aa944bda
George <george.cook.92@hotmail.co.uk>
2016-08-01 14:07:17 +0100

    adding log for delete route to try and fix bug - 2

--- controllers/post.js
+++ controllers/post.js
@@ -83,9 +83,9 @@ exports.changeOfflineStatus = function(req,res,next) {
 }
 
 exports.deletePost = function(req,res,next) {
-  const post_id = req.body.post_id;
+  var post_id = req.body.post_id;
   const query = { "_id": post_id };
-  console.log(query);
+  console.log(req.body);
   Post.remove( query, function(err){
     if(err) return next(err);
     res.json({success: true});
